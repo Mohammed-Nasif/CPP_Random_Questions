6932a8cd705835ed11fd0f5d0cb80adf7df9db40
Mohammed Nasif <99568326+Mohammed-Nasif@users.noreply.github.com>
2022-08-13 15:28:02 +0200

    Create Check IF Prime Number.cpp

---
+++ Check IF Prime Number.cpp
@@ -0,0 +1,24 @@
+#include<iostream.h>
+#include<conio.h>
+int main() {
+    // T => 84 F=> 70
+    int Postive_Num;
+    char Boolean[] = {84}; // True
+    cout<<"Please Enter A +ve Number: "<<std::endl;
+    cin>>Postive_Num;
+    if(Postive_Num == 1 || Postive_Num == 0){
+        Boolean[0] = 70; // Changed To False
+    }
+    for(int i = 2; i < Postive_Num / 2; i++){
+        if(Postive_Num % i == 0){
+            Boolean[0] = 70; // Changed To False
+            break;
+        }
+    }
+    if(Boolean[0] == 84){
+          cout<<Postive_Num<<": Is A Prime Number"<<endl;
+    }else{
+          cout<<Postive_Num<<": Not A Prime Number"<<endl;
+    }
+    return 0;
+}
